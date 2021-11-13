6b37d072cc15abff8b0542f4718e66bfb78df4f1
Ya-Va-A <yavalery@gmail.com>
2021-11-13 21:39:16 +0300

    Magic comments

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,5 +1,61 @@
-﻿Console.Clear();
-//Console.SetCursorPosition(10, 4);
+﻿// Console.Clear();
+// //Console.SetCursorPosition(10, 4);
+// //Console.WriteLine("+");
+
+// int xa = 40, ya = 1,
+//     xb = 1, yb = 30,
+//     xc = 80, yc = 30;
+
+// Console.SetCursorPosition(xa, ya);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xb, yb);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xc, yc);
+// Console.WriteLine("+");
+
+
+// int x = xa, y = xb;
+
+// int count = 0;
+
+// while (count < 10000)
+// {
+//     int what = new Random().Next(0, 3); // [0;3) 0 1 2
+//     if (what == 0)
+//     {
+//         x = (x + xa) / 2;
+//         y = (y + ya) / 2;
+//     }
+
+//     if (what == 1)
+//     {
+//         x = (x + xb) / 2;
+//         y = (y + yb) / 2;
+//     }
+
+//     if (what == 2)
+//     {
+//         x = (x + xc) / 2;
+//         y = (y + yc) / 2;
+//     }
+
+//     Console.SetCursorPosition(x, y);
+//     Console.WriteLine("+");
+//     count++;
+// }
+
+//СИНТАКСИС
+// while  (условие продолжения)
+//{Набор действий}
+//int count = 0;
+//while (count<100)
+//{набор действий
+//count = count +1}
+
+Console.Clear();
+//Console.SetCursorPosition(10, 4); //jтступ курсора
 //Console.WriteLine("+");
 
 int xa = 40, ya = 1,
@@ -15,33 +71,28 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-
 int x = xa, y = xb;
-
 int count = 0;
-
-while (count < 10000)
+while(count < 100000)
 {
-    int what = new Random().Next(0, 3); // [0;3) 0 1 2
-    if (what == 0)
+    int what = new Random().Next(0, 3); // [0;3)
+    if(what == 0)
     {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        x = (x+xa) / 2;
+        y = (y+ya) / 2;
     }
-
-    if (what == 1)
+    if(what == 1)
     {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;
+        x = (x+xb) / 2;
+        y = (y+yb) / 2;
     }
-
-    if (what == 2)
+    if(what == 2)
     {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
+        x = (x+xc) / 2;
+        y = (y+yc) / 2;
     }
-
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
-    count++;
+Console.SetCursorPosition(x, y);
+Console.WriteLine("+");
+count++;
 }
+
