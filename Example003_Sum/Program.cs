2e05b697c8563429b3cddb25f23c0d121a47ac6d
Ya-Va-A <yavalery@gmail.com>
2021-11-13 20:43:59 +0300

    int

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,12 @@
-﻿int numberA = new Random().Next(1, 100); // 1 2 3 4 ... 9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(111, 200);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
+﻿//определим переменную 
+int numberA=3;
+int numberB = 5;
+int result = numberA +numberB;
 Console.WriteLine(result);
+
+// int numberA = new Random().Next(1, 100); // 1 2 3 4 ... 9
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(111, 200);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
