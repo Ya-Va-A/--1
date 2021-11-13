8ef57c0bc62d20cddf864c9aa49b4f7a5ff6e668
Ya-Va-A <yavalery@gmail.com>
2021-11-13 20:27:44 +0300

    Comments

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
+﻿Console.Write("Введите ваше имя ");//приглашение ко вводу
+string username = Console.ReadLine();//string - переменная, тип данных
 Console.Write("Привет, ");
 Console.Write(username);
+//чтобы все выводилось в одну строку вместо Console.WriteLine можно указать Console.Write
