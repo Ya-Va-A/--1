9fac4ad6525ec0659feb115c86d53a08cb89ced9
Ya-Va-A <yavalery@gmail.com>
2021-11-18 13:12:16 +0300

    Lection task008

--- Selfmade/Program.cs
+++ Selfmade/Program.cs
@@ -29,44 +29,57 @@
 
 // 3.По заданному номеру дня недели вывести его название
 
-Console.WriteLine ("Enter day number");
-string daynumber = Console.ReadLine();
-if (daynumber.ToLower() == "1")
-{
-    Console.WriteLine("Monday");
-}
-if (daynumber.ToLower() == "2")
-{
-    Console.WriteLine("Tuesday");
-}
-if (daynumber.ToLower() == "3")
-{
-    Console.WriteLine("Wednesday");
-}
-if (daynumber.ToLower() == "4")
-{
-    Console.WriteLine("Thursday");
-}
-if (daynumber.ToLower() == "5")
-{
-    Console.WriteLine("Friday");
-}
-if (daynumber.ToLower() == "6")
-{
-    Console.WriteLine("Suterday");
-}
-if (daynumber.ToLower() == "7")
-{
-    Console.WriteLine("Sunday");
-}
+// Console.WriteLine ("Enter day number");
+// string daynumber = Console.ReadLine();
+// if (daynumber.ToLower() == "1")
+// {
+//     Console.WriteLine("Monday");
+// }
+// if (daynumber.ToLower() == "2")
+// {
+//     Console.WriteLine("Tuesday");
+// }
+// if (daynumber.ToLower() == "3")
+// {
+//     Console.WriteLine("Wednesday");
+// }
+// if (daynumber.ToLower() == "4")
+// {
+//     Console.WriteLine("Thursday");
+// }
+// if (daynumber.ToLower() == "5")
+// {
+//     Console.WriteLine("Friday");
+// }
+// if (daynumber.ToLower() == "6")
+// {
+//     Console.WriteLine("Suterday");
+// }
+// if (daynumber.ToLower() == "7")
+// {
+//     Console.WriteLine("Sunday");
+// }
 
-// int Monday = 1;
-// int Tuesday = 2;
-// int Wednesday = 3;
-// int Thursday = 4;
-// int Friday = 5;
-// int Suterday = 6;
-// int Sunday = 7;
+// Var 1
+// Console.WriteLine("Enter number from 1 to 7");
+//   string DayName (int daynumber)
+// {
+//        string[] dayname = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+//        return dayname[daynumber - 1];
+// } 
+// int daynumber = new Random().Next(1,8);
+// Console.WriteLine($"{daynumber} = {DayName(daynumber)}");
 
 
+// 4. Найти максимальное из трех чисел
 
+int a = 5;
+int b = 9;
+int c = 2;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = ");
+Console.WriteLine (max);
